c2bf1470547127cd14e09b67a2915ddbf9565cf9
Giovanni Foiani <giovanni.foiani@fancypixel.it>
2023-12-02 08:21:23 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'indent': 'off',
+    'max-len': 'off',
+    'no-void': 'off',
+    'no-console': 'off',
+    'no-useless-constructor': 'off',
+    'no-use-before-define': 'off',
+    'no-shadow': 'off',
+    'object-curly-newline': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+
+    '@typescript-eslint/indent': [
+      "error",
+      2,
+      {
+        "ignoredNodes": [
+          "FunctionExpression > .params[decorators.length > 0]",
+          "FunctionExpression > .params > :matches(Decorator, :not(:first-child))",
+          "ClassBody.body > PropertyDefinition[decorators.length > 0] > .key"
+        ]
+      },
+    ],
+  },
+};
---
+++ README.md
@@ -0,0 +1,13 @@
+# Advent of Code 2020
+
+> Advent of Code is an Advent calendar of small programming puzzles for a variety of skill sets and skill levels that can be solved in any programming language you like.
+
+You can find the Advent of Code 2023 [here](https://adventofcode.com/2023/).
+
+In this repo I'll be posting my solutions written in NodeJS.
+
+## Run a puzzle
+
+```NodeJS
+node -r ts-node/register src/[day]/index.ts
+```
