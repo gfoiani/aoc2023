0d1a733c2ceadc84aad62c317308e88dad79a00f
Giovanni Foiani <giovanni.foiani@fancypixel.it>
2023-12-05 20:33:44 +0100

    Add 03 b (in progress)

---
+++ src/03/a/index.ts
@@ -0,0 +1,54 @@
+import fs from 'fs';
+import path from 'path';
+import { getPuzzleName } from '../../utils';
+
+const puzzle = `Puzzle ${getPuzzleName(__dirname)}`;
+
+const filePath = path.join(__dirname, '..', 'input.txt');
+
+console.time(puzzle);
+const input = fs.readFileSync(filePath, { encoding: 'utf-8' }).trim().split('\n');
+const lines = input.length;
+
+const regex = /(\d+)/g;
+const notNumberOrDotRegex = /[^0-9.]/;
+
+function extractComponents(idx: number): number {
+  const line = input[idx];
+  const prevLine = ((idx - 1) >= 0) ? input[idx - 1] : undefined;
+  const nextLine = ((idx + 1) < lines) ? input[idx + 1] : undefined;
+  console.log('prev', prevLine);
+  console.log('line', line);
+  console.log('next', nextLine);
+  let sum = 0;
+  let match = regex.exec(line);
+  while (match !== null) {
+    let isComponent = false;
+    const { index } = match;
+    const value = match['0'];
+    const nextToChars = `${line[index - 1]}${line[index + value.length]}`;
+    isComponent = notNumberOrDotRegex.test(nextToChars);
+    // if adjacent characters are not symbols check the previous line
+    if (!isComponent && prevLine) {
+      const prevLineChars = prevLine.substring(index - 1, index + value.length + 1);
+      isComponent = notNumberOrDotRegex.test(prevLineChars);
+    }
+    // if adjacent characters are not symbols check the next line
+    if (!isComponent && nextLine) {
+      const nextLineChars = nextLine.substring(index - 1, index + value.length + 1);
+      isComponent = notNumberOrDotRegex.test(nextLineChars);
+    }
+    console.log(`isComponent ${value}`, isComponent);
+    if (isComponent) { sum += parseInt(value, 10); }
+    match = regex.exec(line);
+  }
+
+  console.log('line sum', sum);
+  return sum;
+}
+
+// Calculate the sum of all valid games
+const res = input.reduce((acc, line: string, index: number) => acc + extractComponents(index), 0);
+
+console.log('Result:', res);
+console.timeEnd(puzzle);
